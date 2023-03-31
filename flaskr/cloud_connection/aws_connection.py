21989b002896a68548b1dab747915df2e8f8801e
José Maria reis dos Santos <80988756+Jose-dos-Santos@users.noreply.github.com>
2023-03-31 20:09:37 -0300

    create task scheduler to tranfer files (#30)
    Co-authored-by: Zaion Felippe <81268185+ZaionKun@users.noreply.github.com>

--- flaskr/cloud_connection/aws_connection.py
+++ flaskr/cloud_connection/aws_connection.py
@@ -2,8 +2,9 @@ import boto3
 
 def connect_to_s3():
 
-    aws_access_key_id = 'AKIA2J3TPQFIXFCYTP5Y'
-    aws_secret_access_key = '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'
+    aws_access_key_id = 'AKIA2J3TPQFIQ32P2DEZ'
+    aws_secret_access_key = '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'
+    
     bucket_name = 'techninja'
 
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
---
+++ flaskr/job_scheduler.py
@@ -0,0 +1,23 @@
+from flask_apscheduler import APScheduler
+from flaskr.services.file import FileModelService
+
+def task_tranfer_files():
+    fm = FileModelService()
+    fm.transfer_files()
+    print(fm)
+
+def init_apscheduler(app, scheduler_enabled=False):
+    if scheduler_enabled:
+        app.config['SCHEDULER_EXECUTORS'] = {"default": {"type": "threadpool", "max_workers": 10}}
+        app.config['SCHEDULER_JOB_DEFAULTS'] = {"coalesce": False, "max_instances": 3}
+        app.config['SCHEDULER_API_ENABLED'] = True
+
+        scheduler = APScheduler()
+        scheduler.init_app(app)
+        # Set and config task jobs
+        scheduler.add_job(id='task_tranfer_files', func=task_tranfer_files, trigger='cron', minute='*/1')
+                         
+
+        print("Job task_tranfer_files registered and working...")
+        print(scheduler.get_jobs())
+        scheduler.start()
