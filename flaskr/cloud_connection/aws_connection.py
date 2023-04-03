228d5b9a9c17c1f46fb5b2ba6c639a83afc44f5f
Zaion Arruda <Zaion.Arruda@ball.com>
2023-04-03 16:24:19 -0300

    remove bucket name

--- flaskr/cloud_connection/aws_connection.py
+++ flaskr/cloud_connection/aws_connection.py
@@ -5,7 +5,7 @@ def connect_to_s3():
     aws_access_key_id = ''
     aws_secret_access_key = ''
     
-    bucket_name = 'techninja'
+    bucket_name = ''
 
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
