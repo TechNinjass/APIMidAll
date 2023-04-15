ae98c363a8d2a89ce3ce0a01280f8c97bd83324e
Jose-dos-Santos <jose.jc.santos@hotmail.com>
2023-04-15 15:28:40 -0300

    Initial config db

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,16 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
+import flaskr.config_app as ca
 
 db_instance = SQLAlchemy()
 
 def config_sql_alchemy(app):
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///project.db"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_ECHO'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ca.MIDALL_DATABASE_URL
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = ca.SQLALCHEMY_TRACK_MODIFICATIONS
+    app.config['SQLALCHEMY_ECHO'] = ca.SQLALCHEMY_ECHO
 
     db_instance.app = app
     db_instance.init_app(app)
+
 
 def db_persist(func):
     def persist(*args, **kwargs):
