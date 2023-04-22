281c43ab62cf8b313c4278de16af4da74ae1c5e8
José Maria reis dos Santos <80988756+Jose-dos-Santos@users.noreply.github.com>
2023-04-22 14:50:16 -0300

    test save files in transfer (#55)

---
+++ tests/test_azure.py
@@ -0,0 +1,13 @@
+from flaskr.db import db_instance
+from flaskr.models.file_transfer import FileTransferModel
+from flaskr.cloud.azure import Azure
+
+class TestAzure:
+
+    def test_save_new_file_transfer(self, mocker):
+        mock_session = mocker.Mock()
+        mocker.patch.object(db_instance, 'session', mock_session)
+        file_transfer = FileTransferModel(name='test_file', size=100, format='pdf', date_upload='2022-01-01', data_transfer='2022-01-02')
+        file_transfer.save()
+        mock_session.merge.assert_called_once_with(file_transfer)
+        mock_session.commit.assert_called_once()
