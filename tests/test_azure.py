9e2acd8fe69cae5bb29c3ad95e99fcc67aa947b9
Andrew.Santos <andrew.santos@spsconsultoria.com.br>
2023-04-22 15:18:18 -0300

    Fiz requirement and indentation

--- tests/test_azure.py
+++ tests/test_azure.py
@@ -3,24 +3,39 @@ from flaskr.models.file_transfer import FileTransferModel
 from flaskr.cloud.azure import Azure
 import pickle
 
-class TestAzure:
 
+class TestAzure:
     def test_save_new_file_transfer(self, mocker):
         mock_session = mocker.Mock()
-        mocker.patch.object(db_instance, 'session', mock_session)
-        file_transfer = FileTransferModel(name='test_file', size=100, format='pdf', date_upload='2022-01-01', data_transfer='2022-01-02')
+        mocker.patch.object(db_instance, "session", mock_session)
+        file_transfer = FileTransferModel(
+            name="test_file",
+            size=100,
+            format="pdf",
+            date_upload="2022-01-01",
+            data_transfer="2022-01-02",
+        )
         file_transfer.save()
         mock_session.merge.assert_called_once_with(file_transfer)
         mock_session.commit.assert_called_once()
 
     def test_list_files_empty_container(self, mocker):
-        credentials = {"account_name": "test_account", "account_key": "test_key", "container_name": "test_container"}
+        credentials = {
+            "account_name": "test_account",
+            "account_key": "test_key",
+            "container_name": "test_container",
+        }
         with open("credentialsAzure.pickle", "wb") as f:
             pickle.dump(credentials, f)
         mock_blob_service_client = mocker.Mock()
         mock_container_client = mocker.Mock()
-        mock_blob_service_client.get_container_client.return_value = mock_container_client
-        mocker.patch("azure.storage.blob.BlobServiceClient.from_connection_string", return_value=mock_blob_service_client)
+        mock_blob_service_client.get_container_client.return_value = (
+            mock_container_client
+        )
+        mocker.patch(
+            "azure.storage.blob.BlobServiceClient.from_connection_string",
+            return_value=mock_blob_service_client,
+        )
 
         mock_blob_list = []
         mock_container_client.list_blobs.return_value = mock_blob_list
