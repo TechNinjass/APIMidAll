feda92b34f372a8c8d8fa2b07c490cd31e7cf7de
Andrew.Santos <andrew.santos@spsconsultoria.com.br>
2023-04-22 14:56:53 -0300

    Teste test_list_files_empty_container

--- tests/test_azure.py
+++ tests/test_azure.py
@@ -11,3 +11,17 @@ class TestAzure:
         file_transfer.save()
         mock_session.merge.assert_called_once_with(file_transfer)
         mock_session.commit.assert_called_once()
+
+    def test_list_files_empty_container(self, mocker):
+        mock_blob_service_client = mocker.Mock()
+        mock_container_client = mocker.Mock()
+        mock_blob_service_client.get_container_client.return_value = mock_container_client
+        mocker.patch("azure.storage.blob.BlobServiceClient.from_connection_string", return_value=mock_blob_service_client)
+
+        mock_blob_list = []
+        mock_container_client.list_blobs.return_value = mock_blob_list
+
+        azure = Azure()
+        files = azure.list_files()
+
+        assert files == []
