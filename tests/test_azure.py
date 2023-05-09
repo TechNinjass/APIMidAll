b4c86050c7ec7ee1b9ecc9dec9de86f2e8485670
Jose-dos-Santos <jose.jc.santos@hotmail.com>
2023-05-08 22:00:43 -0300

    create test azure

--- tests/test_azure.py
+++ tests/test_azure.py
@@ -1,8 +1,7 @@
 from flaskr.db import db_instance
 from flaskr.models.file_transfer import FileTransferModel
 from flaskr.cloud.azure import Azure
-import pickle
-
+import pytest
 
 class TestAzure:
     def test_save_new_file_transfer(self, mocker):
@@ -14,33 +13,18 @@ class TestAzure:
             format="pdf",
             date_upload="2022-01-01",
             data_transfer="2022-01-02",
+            status = 'ok'
         )
         file_transfer.save()
         mock_session.merge.assert_called_once_with(file_transfer)
         mock_session.commit.assert_called_once()
 
-    def test_list_files_empty_container(self, mocker):
-        credentials = {
-            "account_name": "test_account",
-            "account_key": "test_key",
-            "container_name": "test_container",
-        }
-        with open("credentialsAzure.pickle", "wb") as f:
-            pickle.dump(credentials, f)
-        mock_blob_service_client = mocker.Mock()
-        mock_container_client = mocker.Mock()
-        mock_blob_service_client.get_container_client.return_value = (
-            mock_container_client
-        )
-        mocker.patch(
-            "azure.storage.blob.BlobServiceClient.from_connection_string",
-            return_value=mock_blob_service_client,
-        )
+    
+    def test_connection_azure_invalid_credentials_file(self, mocker):
+        
+        mocker.patch('os.path.exists', return_value=False)
 
-        mock_blob_list = []
-        mock_container_client.list_blobs.return_value = mock_blob_list
-
+      
         azure = Azure()
-        files = azure.list_files()
-
-        assert files == []
+        with pytest.raises(Exception):
+            azure.connection_azure()
