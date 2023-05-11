0a6f2558d3266ed510d11c1f560a50613afed0af
Jose-dos-Santos <jose.jc.santos@hotmail.com>
2023-05-11 20:43:21 -0300

    unit test creation

---
+++ tests/test_azure_connection.py
@@ -0,0 +1,47 @@
+
+import pytest 
+from flaskr.cloud.azure import Azure
+from flaskr.resources.azure_connection import AzureResource
+
+
+import flaskr.cloud.config_credentials as cc
+
+class TestAzureConnection:
+   
+    def test_connection_azure_happy(self, mocker):
+        mock_post = mocker.patch.object(AzureResource, 'post')
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {"message": "Conexão realizada com sucesso!"}
+
+        data = {"account_name": "test_account", "account_key": "test_key", "container_name": "test_container"}
+        response = AzureResource().post(data=data)
+
+        assert response.status_code == 200
+        assert response.json() == {"message": "Conexão realizada com sucesso!"}
+
+       
+        # Tests that connection to Azure fails when Azure credentials file does not exist.  
+    def test_connection_azure_edge_file_not_exist(self, mocker):
+        mocker.patch('os.path.exists', return_value=False)
+        azure = Azure()
+        with pytest.raises(Exception):
+            azure.connection_azure()        
+    
+    
+    def test_connection_azure_edge(self, mocker):
+        # Mocking Azure connection
+        mock_azure = mocker.patch("flaskr.cloud.azure.Azure.connection_azure")
+        mock_azure.side_effect = Exception("Invalid credentials")
+
+        # Mocking AzureResource.post
+        mock_post = mocker.patch.object(AzureResource, "post")
+        mock_post.return_value.status_code = 500
+        mock_post.return_value.json = {"error": "Invalid credentials"}
+
+        # Sending incomplete credentials
+        data = {"account_name": "test_account", "account_key": "test_key"}
+        response = AzureResource().post(data=data)
+
+        assert response.status_code == 500
+        assert response.json == {"error": "Invalid credentials"}
+
